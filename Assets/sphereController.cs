b03535984e7f289e228a5f17e7330bfb50abdffc
Gonzako <al341522@uji.es>
2020-05-21 18:58:45 +0200

    Got Splitscreen input assingment done

---
+++ Assets/sphereController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class sphereController : MonoBehaviour
+{
+
+    Vector2 latestMovInput;
+
+    public void OnMovement(InputAction.CallbackContext ctx)
+    {
+        latestMovInput = ctx.ReadValue<Vector2>();
+    }
+
+    private void Update()
+    {
+        transform.Translate(new Vector3(latestMovInput.x, 0, latestMovInput.y));
+    }
+}
